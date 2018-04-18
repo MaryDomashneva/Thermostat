1c06d080337d737de4bb23adeb1ce1620575af3b
marydomashneva <mary.domashneva@gmail.com>
2018-04-18 14:33:05 +0100

    Termostat control functions and tests added

--- jasmine-standalone-3.1.0/spec/ThermostatSpec.js
+++ jasmine-standalone-3.1.0/spec/ThermostatSpec.js
@@ -1,14 +1,59 @@
 'use strict';
 
 describe('Thermostat', function() {
-  var thermostate;
+  var thermostat;
 
   beforeEach(function(){
-    thermostat = new Thermosat();
-  })
+    thermostat = new Thermostat();
+  });
 
   it('starts with 20 degrees', function() {
-    thermostat.set(temperature);
-    expect(thermostat).toEqual(20)
+    expect(thermostat.temperature).toEqual(20);
+  });
+
+  it('returns current temperature', function() {
+    expect(thermostat.currentTemperature()).toEqual(20);
+  });
+
+  it('sets a new temperature', function() {
+    thermostat.temperatureSet(18);
+    expect(thermostat.currentTemperature()).toEqual(18);
   })
-})
+
+  it('resets temperature back to default 20 points', function() {
+    thermostat.temperatureReSet();
+    expect(thermostat.currentTemperature()).toEqual(20);
+  })
+
+  it('increases temperature by 1 point', function() {
+    expect(thermostat.temperatureUp()).toEqual(21);
+  });
+
+  it('decreases temperature by 1 point', function() {
+    expect(thermostat.temperatureDown()).toEqual(19);
+  });
+
+  it('returns true when power save is on', function() {
+    expect(thermostat.powerSave()).toEqual(true);
+  });
+
+  it('returns false when power save is off', function() {
+    thermostat.temperatureSet(26);
+    expect(thermostat.powerSave()).toEqual(false);
+  });
+
+  it('returns low-usage if current temperature < 18 points', function() {
+    thermostat.temperatureSet(17);
+    expect(thermostat.Usage()).toEqual('low-usage');
+  });
+
+  it('returns low-usage if current 18 > temperature < 25 points', function() {
+    thermostat.temperatureSet(18);
+    expect(thermostat.Usage()).toEqual('medium-usage');
+  });
+
+  it('returns high-usage if current temperature > 25 points', function() {
+    thermostat.temperatureSet(26);
+    expect(thermostat.Usage()).toEqual('high-usage');
+  });
+});
--- jasmine-standalone-3.1.0/src/Thermostat.js
+++ jasmine-standalone-3.1.0/src/Thermostat.js
@@ -1,6 +1,49 @@
+'use strict';
+
 function Thermostat() {
-  var temperature = 20
+  this.temperature = 20;
+  this.poverSave = true;
+  this.usage = 'medium-usage';
 };
-  Thermostat.prototype.set =function(temperature) {
-    this.defoltTemperatureSet = temperature
+
+Thermostat.prototype.temperatureSet = function(temperature) {
+  this.temperature = temperature;
+  return this.temperature
+};
+
+Thermostat.prototype.temperatureReSet = function() {
+  this.temperature = 20;
+  return this.temperature
+};
+
+Thermostat.prototype.temperatureUp = function() {
+  this.temperature += 1;
+  return this.temperature
+};
+
+Thermostat.prototype.temperatureDown = function() {
+  this.temperature -= 1;
+  return this.temperature
+};
+
+Thermostat.prototype.powerSave = (function() {
+  if(this.temperature <= 25) {
+    return this.poverSave = true;
+  } else {
+    return this.poverSave = false;
+  }
+});
+
+Thermostat.prototype.Usage = (function() {
+  if(this.temperature < 18) {
+    return this.usage = 'low-usage';
+  } else if(this.temperature <= 25 && this.temperature >= 18) {
+    return this.usage = 'medium-usage';
+  } else {
+    return this.usage = 'high-usage';
   };
+});
+
+Thermostat.prototype.currentTemperature = function() {
+  return this.temperature;
+};
