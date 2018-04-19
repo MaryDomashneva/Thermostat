df7d8a54aa6a81f5e3363cf379745563b19f500c
marydomashneva <mary.domashneva@gmail.com>
2018-04-19 22:59:12 +0100

    css methode name chane

--- jasmine-standalone-3.1.0/src/weather.js
+++ jasmine-standalone-3.1.0/src/weather.js
@@ -8,5 +8,3 @@ function Wether() {
 Weather.prototype.knowTemperature = function(city) {
   this.city = city
 }
-
-// #http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={c8df41c4038bdc5922376af535474baf}
