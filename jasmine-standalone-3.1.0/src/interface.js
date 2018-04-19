5b38a002c719639e64d351b926c00e4b635751f2
marydomashneva <mary.domashneva@gmail.com>
2018-04-19 22:52:50 +0100

    Added styles and weather icone

--- jasmine-standalone-3.1.0/src/interface.js
+++ jasmine-standalone-3.1.0/src/interface.js
@@ -34,8 +34,9 @@ $(document).ready(function() {
 
   $('#currentCity').change(function() {
     var city = $('#currentCity').val();
-   $.get('http://api.openweathermap.org/data/2.5/weather?id=' + city + '&APPID=c8df41c4038bdc5922376af535474baf&units=metric', function(data) {
-     $('#currentWeather').text(data.main.temp)
-   })
+    $.get('http://api.openweathermap.org/data/2.5/weather?id=' + city + '&APPID=c8df41c4038bdc5922376af535474baf&units=metric', function(data) {
+      $('#currentWeather').text(data.main.temp)
+      $('#temperatureIcon').attr('src', `http://openweathermap.org/img/w/${data.weather[0].icon}.png`)
+    })
   })
 });
